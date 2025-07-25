540c75bf9c3050f9bb4cd7181ff9fa5e6ac662ab
yaim0425 <yaim0425@gmail.com>
2025-07-25 17:08:17 -0500

    Corrección del ancho del GUI

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -255,7 +255,7 @@ function This_MOD.load_styles()
     }
     Styles[Prefix .. "drop_down_channel"] = {
         type = "dropdown_style",
-        width = (296 + 32) + 32
+        width = 296 + 64
     }
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
